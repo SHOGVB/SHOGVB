3c7bd04d94c66805e055830dd104a5a190d79e58
MichaelSwan <mwswan99@gmail.com>
2026-09-04 09:48:23 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+// =========================================
+// MOBILE NAVIGATION
+// =========================================
 
+const menuToggle = document.getElementById("menuToggle");
+const navigation = document.getElementById("navigation");
+
+if (menuToggle && navigation) {
+
+    menuToggle.addEventListener("click", function () {
+
+        navigation.classList.toggle("show");
+
+    });
+
+}
+
+
+// =========================================
+// CLOSE MOBILE MENU AFTER CLICKING A LINK
+// =========================================
+
+const navigationLinks =
+    document.querySelectorAll(".navigation a");
+
+navigationLinks.forEach(function(link) {
+
+    link.addEventListener("click", function() {
+
+        navigation.classList.remove("show");
+
+    });
+
+});
