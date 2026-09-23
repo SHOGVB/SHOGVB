71b424acd3ac1e3bf2f38eb65256ba8c47b9cefc
MichaelSwan <mwswan99@gmail.com>
2026-09-23 13:11:52 -0500

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL =
+    "https://qiqwcrlndqokhkqkliuc.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_vQz4cFTdkbfu0eIqyfnGXQ_5-MKe9zn";
+
+const sb = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
